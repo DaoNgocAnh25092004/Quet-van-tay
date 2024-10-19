a8ad50fe359c8e5cb07f80da649e32ca07065882
DaoNgocAnh25092004 <Daongocanh25042004@gmail.com>
2024-10-19 22:39:15 +0700

    update

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-onload = () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const screenBlur = document.querySelector(".screen-blur");
+  const audio = document.getElementById("background-audio");
+  const playBtn = document.getElementById("fingerprint");
+
+  playBtn.addEventListener("click", () => {
+    screenBlur.style.display = "none";
+    audio.play();
+
+    // Logic để xóa lớp 'not-loaded' sau khi nhấn nút playBtn
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
       clearTimeout(c);
-    }, 1000);
-  };
+    }, 1500);
+  });
+});
